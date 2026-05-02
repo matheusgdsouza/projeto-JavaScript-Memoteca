a74522da26c0508e51acfeff96ed751772d5138d
Matheus de Souza <matheusgds3011@gmail.com>
2026-05-02 14:57:00 -0300

    Feat: Implementando Axios para requisições HTTP

--- js/api.js
+++ js/api.js
@@ -4,9 +4,8 @@ const api = {
     // Função criada para buscar dados da API, utilizando o método GET
     async buscarPensamentos() {
         try {
-            const response = await fetch(urlBaseApi);
-            const data = await response.json();
-            return data;
+            const response = await axios.get(urlBaseApi);
+            return await response.data;
         }    
         catch (error) {
             alert('Erro ao buscar pensamentos: ' + error.message);
@@ -18,15 +17,8 @@ const api = {
     // Função criada para enviar dados para a API, utilizando o método POST
     async salvarPensamento(pensamento) {
         try {
-            const response = await fetch(urlBaseApi, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(pensamento)
-            });
-            const data = await response.json();
-            return data;
+            const response = await axios.post(urlBaseApi, pensamento);
+            return await response.data;
         }    
         catch (error) {
             alert('Erro ao salvar pensamento: ' + error.message);
@@ -37,9 +29,8 @@ const api = {
     // Função criada para buscar um pensamento específico da API, utilizando o método GET
     async buscarPensamento(id) {
         try {
-            const response = await fetch(`${urlBaseApi}/${id}`);
-            const data = await response.json();
-            return data;
+            const response = await axios.get(`${urlBaseApi}/${id}`);
+            return await response.data;
         }    
         catch (error) {
             alert('Erro ao buscar pensamento: ' + error.message);
@@ -51,15 +42,8 @@ const api = {
     // Função criada para editar um pensamento específico da API, utilizando o método PUT
     async editarPensamento(pensamento) {
         try {
-            const response = await fetch(`${urlBaseApi}/${pensamento.id}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(pensamento)
-            });
-            const data = await response.json();
-            return data;
+            const response = await axios.put(`${urlBaseApi}/${pensamento.id}`, pensamento);
+            return await response.data;
         }    
         catch (error) {
             alert('Erro ao editar pensamento: ' + error.message);
@@ -70,15 +54,7 @@ const api = {
     // Função criada para deletar um pensamento específico de API, utilizando o método DELETE
     async deletarPensamento(pensamento) {
         try {
-            const response = await fetch(`${urlBaseApi}/${pensamento.id}`, {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(pensamento)
-            });
-            const data = await response.json();
-            return data;
+            const response = await axios.delete(`${urlBaseApi}/${pensamento.id}`, pensamento);
         }    
         catch (error) {
             alert('Erro ao deletar pensamento: ' + error.message);
